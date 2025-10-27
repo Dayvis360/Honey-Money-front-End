c7bd92f4bff83b197edd0b717001ce088e2ceb9e
dayvis360 <dayvis209@gmail.com>
2025-10-26 23:58:49 -0300

    envio post de datos front end

--- sigin/register.js
+++ sigin/register.js
@@ -1,5 +1,22 @@
-document.getElementById("register-form").addEventListener("submit", (e)=>{
+document.getElementById("register-form").addEventListener("submit", async (e)=>{
     e.preventDefault();
-    console.log(e.target.nombre.value)
+    const res = await fetch("RUTA", {
+        method: "Post",
+        headers:{
+            "Content_Type" : "applicacion/json"
+        },
+        body: JSON.stringify({
+            nombre: e.target.nombre.value, 
+            apellido: e.target.apellido.value, 
+            dni: e.target.dni.value, 
+            f_nac:e.target.fecha_de_nacimiento.value, 
+            correo: e.target.correo.value, 
+            telefono: e.target.telefono.value, 
+            contraseña: e.target.contraseña.value
+        })
+    })
+        
+    
+    
 })
-console.log("HOLA")
+console.log("fin")
