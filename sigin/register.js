685433230f0c7a375ea8d09c83b19e33126a6f95
dayvis360 <dayvis209@gmail.com>
2025-10-26 23:46:41 -0300

    a

--- sigin/register.js
+++ sigin/register.js
@@ -1,5 +1,5 @@
 document.getElementById("register-form").addEventListener("submit", (e)=>{
     e.preventDefault();
-    console.log(e.target.children.nombre.value)
+    console.log(e.target.nombre.value)
 })
 console.log("HOLA")
